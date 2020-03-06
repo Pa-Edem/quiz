b6bbe277044581710188721b151ddd86a72fe926
Sergey Palagin <paedem.01@gmail.com>
2020-03-07 00:08:14 +0300

    finished QuizList with redux

--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
 import './QuizList.scss';
 import { NavLink } from 'react-router-dom';
-import axios from '../../axios/axiosQuiz';
 
 import Loader from '../../components/UI/Loader/Loader';
+import { connect } from 'react-redux';
+import { fetchQuizes } from '../../store/actions/quiz';
 
-export default class QuizList extends Component {
-	state = {
-		quizes: [],
-		loading: true,
-	};
-
+class QuizList extends Component {
 	renderQuizes() {
-		return this.state.quizes.map(quiz => {
+		return this.props.quizes.map(quiz => {
 			return (
 				<li key={quiz.id}>
 					<NavLink to={'/quiz/' + quiz.id}>{quiz.name}</NavLink>
@@ -21,21 +17,8 @@ export default class QuizList extends Component {
 		});
 	}
 
-	async componentDidMount() {
-		try {
-			const response = await axios.get('/quizes.json');
-
-			const quizes = [];
-			Object.keys(response.data).forEach((key, index) => {
-				quizes.push({
-					id: key,
-					name: `Test number ${index + 1}`,
-				});
-			});
-			this.setState({ quizes, loading: false });
-		} catch (error) {
-			console.error(error);
-		}
+	componentDidMount() {
+		this.props.fetchQuizes();
 	}
 
 	render() {
@@ -44,9 +27,28 @@ export default class QuizList extends Component {
 				<div>
 					<h1>Test list</h1>
 
-					{this.state.loading ? <Loader /> : <ul>{this.renderQuizes()}</ul>}
+					{this.props.loading && this.props.quizes.length !== 0 ? (
+						<Loader />
+					) : (
+						<ul>{this.renderQuizes()}</ul>
+					)}
 				</div>
 			</div>
 		);
 	}
 }
+
+function mapStateToProps(state) {
+	return {
+		quizes: state.quiz.quizes,
+		loading: state.quiz.loading,
+	};
+}
+
+function mapDispatchToProps(dispatch) {
+	return {
+		fetchQuizes: () => dispatch(fetchQuizes()),
+	};
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(QuizList);
